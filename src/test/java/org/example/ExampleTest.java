244b17230e1271557bdc4822fbbc6b22b366a733
mariusflores <mfloresdal@gmail.com>
2026-05-02 00:02:54 +0200

    example class and test

---
+++ src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleAssertTrue(){
+        int test = 1;
+
+        assertEquals(1, test);
+    }
+}
